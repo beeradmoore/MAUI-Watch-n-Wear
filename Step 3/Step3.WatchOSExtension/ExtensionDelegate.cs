623f1111fb954ff086c5cc3fbb0613feca3464a4
Brad <brad@4pi.com.au>
2022-05-02 18:29:19 +1000

    Added non-functional wearOS app

---
+++ Step 3/Step3.WatchOSExtension/ExtensionDelegate.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+using Foundation;
+using WatchKit;
+
+namespace Step3.WatchOSExtension
+{
+    public class ExtensionDelegate : WKExtensionDelegate
+    {
+        public ExtensionDelegate()
+        {
+        }
+    }
+}
+
---
+++ Step 3/Step3.WatchOSExtension/InterfaceController.designer.cs
@@ -0,0 +1,32 @@
+// WARNING
+//
+// This file has been generated automatically by Visual Studio to store outlets and
+// actions made in the UI designer. If it is removed, they will be lost.
+// Manual changes to this file may not be handled correctly.
+//
+using Foundation;
+using System.CodeDom.Compiler;
+
+namespace Step3.WatchOSExtension
+{
+	[Register ("InterfaceController")]
+	partial class InterfaceController
+	{
+		[Outlet]
+		WatchKit.WKInterfaceLabel displayLabel { get; set; }
+
+		[Action ("decClicked")]
+		partial void decClicked ();
+
+		[Action ("incClicked")]
+		partial void incClicked ();
+		
+		void ReleaseDesignerOutlets ()
+		{
+			if (displayLabel != null) {
+				displayLabel.Dispose ();
+				displayLabel = null;
+			}
+		}
+	}
+}
