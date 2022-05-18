35e988ab6cf5acddce864facee4ba6174ed9fa28
Brad <brad@4pi.com.au>
2022-05-18 20:33:34 +1000

    Added wearOS app for step 3 as a net6.0-android app, made reference to KMM for step 5, added notes for each step as we go.

--- README.md
+++ README.md
@@ -1,15 +1,16 @@
 # MAUI Watch 'n Wear
 This repository dedicated to exploring adding watchOS and Wear OS applications in MAUI iOS and Android builds. 
 
-To accomplish this we will attempt various different steps to slowly add more and more functionality. A rough outline of how this will go is:
-
 ### Status 
 | Step   | iOS Status | Android Status |
 |--------|------------|----------------|
 | Step 1 | ✅         | ✅              |
 | Step 2 | ✅         | ✅              |
-| Step 3 | Can't get MAUI to reference Step3.WatchOSApp | Currently working but not deploying in a single apk (may not be required) |
-| Step 4 | Untested         | Untested               |
+| Step 3 | ❌         | ✅              |
+| Step 4 | Untested   | Untested       |
+| Step 5 | Untested   | Untested       |
+
+
 
 
 ### Basic app concept
@@ -17,22 +18,37 @@ The app is intended to be simple and demonstrate basic functionality and communi
 
 If the app is considered a "score keeper" type app it may also possible to distribute to the App Store and Google Play for live testing.
 
+To accomplish this we will attempt various different steps to slowly add more and more functionality. A rough outline of how this will go is:
+
 ### Step 1
 Create a Xamarin.iOS and Xamarin.Android project with the accompanying wearable app with the functionality as above.
+
+#### Result
+Works like a charm as it has for some time.
 
 ### Step 1a (optional)
 Test the functionality of an iPhone with a paired Wear OS device. This appears to be possible, see the Android developer docs on independent vs standalone Wear OS app below.
 
+#### Result
+Not attempted yet.
+
 ### Step 2
 Create a Xamarin.Forms application and which has included in the wearable projects from step 1.
+
+#### Result
+Works like a charm as it has for some time.
 
 ### Step 3
 Create a MAUI application and add relevant tooling to bundle the Xamarin wearable app from step 1.
 
+#### Result
+Thanks to some [online help](https://twitter.com/ivictorhugo/status/1526561045698969604) it is possible to build a net6.0-android app for a wearOS device, which is a different app target type than what was used in step 1 but this is preferred. Unfortunately the MAUI app can't reference the Step3.WatchOSApp project and there is no net6.0-watchos support.
+
 ### Step 4
 Because Xamarin will reach end of support at some point ([2 years after final release](https://dotnet.microsoft.com/en-us/platform/support/policy/xamarin)) also test building of a native Obj-C/Swift watchOS app and a native Java/Kotlin Wear OS app bundlded within the MAUI project created in step 3.
 
-
+### Step 5
+Implement both wearable apps with [Kotlin Multiplatform Mobile](https://kotlinlang.org/lp/mobile/) to keep a single logic base. 
 
 
 
--- Step 3/Step3.WearOSApp/MainActivity.cs
+++ Step 3/Step3.WearOSApp/MainActivity.cs
@@ -1,125 +1,133 @@
-﻿using System;
-
+using System;
 using Android.App;
 using Android.Widget;
 using Android.OS;
-using Android.Support.Wearable.Activity;
+using Android.Support;
+using AndroidX.Wear.Activity;
 using Android.Gms.Wearable;
 using Android.Gms.Common;
 using Android.Gms.Common.Apis;
 using System.Threading.Tasks;
 using System.Collections.Generic;
 
-namespace WearOSApp
-{
-    [Activity(Label = "@string/app_name", MainLauncher = true)]
-    public class MainActivity : WearableActivity,
+namespace Step3.WearOSApp;
+
+[Activity(Label = "@string/app_name", MainLauncher = true)]
+public class MainActivity : Activity,
         MessageClient.IOnMessageReceivedListener,
         CapabilityClient.IOnCapabilityChangedListener
+{
+    int count = 0;
+    TextView? textView;
+    ICollection<INode> nodes = new List<INode>();
+
+    protected override void OnCreate(Bundle? savedInstanceState)
     {
-        int count = 0;
-        TextView textView;
-        ICollection<INode> nodes;
+        base.OnCreate(savedInstanceState);
 
-        protected override void OnCreate(Bundle bundle)
+        // Set our view from the "main" layout resource
+        SetContentView(Resource.Layout.activity_main);
+
+        textView = FindViewById<TextView>(Resource.Id.text_view_display);
+
+
+        var decButton = FindViewById<Button>(Resource.Id.dec_button);
+        if (decButton != null)
         {
-            base.OnCreate(bundle);
-            SetContentView(Resource.Layout.activity_main);
-
-            textView = FindViewById<TextView>(Resource.Id.text_view_display);
-            SetAmbientEnabled();
-
-
-            var decButton = FindViewById<Button>(Resource.Id.dec_button);
-            decButton.Click += (object sender, EventArgs e) =>
+            decButton.Click += (object? sender, EventArgs e) =>
             {
                 --count;
                 UpdateTextDisplay();
                 SyncCountToPhone();
             };
+        }
 
-            var incButton = FindViewById<Button>(Resource.Id.inc_button);
-            incButton.Click += (object sender, EventArgs e) =>
+        var incButton = FindViewById<Button>(Resource.Id.inc_button);
+        if (incButton != null)
+        { 
+            incButton.Click += (object? sender, EventArgs e) =>
             {
                 ++count;
                 UpdateTextDisplay();
                 SyncCountToPhone();
             };
-
-            SetupCapability();
         }
 
-        async Task SetupCapability()
+        SetupCapability();
+    }
+
+
+    async Task SetupCapability()
+    {
+        await WearableClass.GetMessageClient(this).AddListenerAsync(this);
+
+        var capabilityInfo = await WearableClass.GetCapabilityClient(this).GetCapabilityAsync("step3_sync_count", CapabilityClient.FilterReachable);
+        nodes = capabilityInfo.Nodes;
+
+        await WearableClass.GetCapabilityClient(this).AddListenerAsync(this, "step3_sync_count");
+    }
+
+    void UpdateTextDisplay()
+    {
+        RunOnUiThread(() =>
         {
-            await WearableClass.GetMessageClient(this).AddListenerAsync(this);
-
-            var capabilityInfo = await WearableClass.GetCapabilityClient(this).GetCapabilityAsync("step3_sync_count", CapabilityClient.FilterReachable);
-            nodes = capabilityInfo.Nodes;
-
-            await WearableClass.GetCapabilityClient(this).AddListenerAsync(this, "step3_sync_count");
-        }
-
-        void UpdateTextDisplay()
-        {
-            RunOnUiThread(() =>
+            if (textView != null)
             {
                 textView.Text = count.ToString();
-            });
-        }
-
-        async Task SyncCountToPhone()
-        {
-            var data = BitConverter.GetBytes(count);
-            //await UpdateConnectedNodes();
-            System.Diagnostics.Debug.WriteLine($"Watch: Attempting to send to {nodes.Count} nodes");
-            foreach (var node in nodes)
-            {
-                System.Diagnostics.Debug.WriteLine($"Watch: Attempting to send to {node.DisplayName}");
-                var result = await WearableClass.GetMessageClient(this).SendMessageAsync(node.Id, "/count", data);
-                System.Diagnostics.Debug.WriteLine(result);
             }
-        }
-
-
-        /*
-        async Task<ICollection<INode>> UpdateConnectedNodes()
-        {
-            var capabilityInfo = await WearableClass.GetCapabilityClient(this).GetCapabilityAsync("step3_sync_count", CapabilityClient.FilterReachable);
-            System.Diagnostics.Debug.WriteLine($"UpdateConnectedNodes: Found {capabilityInfo.Nodes.Count}");
-            foreach (var node in capabilityInfo.Nodes)
-            {
-                System.Diagnostics.Debug.WriteLine($"UpdateConnectedNodes: {node.DisplayName}");
-            }
-
-            nodes = await WearableClass.GetNodeClient(this).GetConnectedNodesAsync();
-            return nodes;
-        }
-        */
-
-
-        #region CapabilityClient.IOnCapabilityChangedListener
-        public void OnCapabilityChanged(ICapabilityInfo capabilityInfo)
-        {
-            System.Diagnostics.Debug.WriteLine("Watch: OnCapabilityChanged");
-
-            nodes = capabilityInfo.Nodes;
-        }
-        #endregion
-
-
-        #region MessageClient.IOnMessageReceivedListener
-        public void OnMessageReceived(IMessageEvent messageEvent)
-        {
-            System.Diagnostics.Debug.WriteLine("Watch: OnMessageReceived");
-            if (messageEvent.Path == "/count")
-            {
-                var data = messageEvent.GetData();
-                count = BitConverter.ToInt32(data);
-                UpdateTextDisplay();
-            }
-        }
-        #endregion
+        });
     }
+
+    async Task SyncCountToPhone()
+    {
+        var data = BitConverter.GetBytes(count);
+        //await UpdateConnectedNodes();
+        System.Diagnostics.Debug.WriteLine($"Watch: Attempting to send to {nodes.Count} nodes");
+        foreach (var node in nodes)
+        {
+            System.Diagnostics.Debug.WriteLine($"Watch: Attempting to send to {node.DisplayName}");
+            var result = await WearableClass.GetMessageClient(this).SendMessageAsync(node.Id, "/count", data);
+            System.Diagnostics.Debug.WriteLine(result);
+        }
+    }
+
+
+    /*
+    async Task<ICollection<INode>> UpdateConnectedNodes()
+    {
+        var capabilityInfo = await WearableClass.GetCapabilityClient(this).GetCapabilityAsync("step3_sync_count", CapabilityClient.FilterReachable);
+        System.Diagnostics.Debug.WriteLine($"UpdateConnectedNodes: Found {capabilityInfo.Nodes.Count}");
+        foreach (var node in capabilityInfo.Nodes)
+        {
+            System.Diagnostics.Debug.WriteLine($"UpdateConnectedNodes: {node.DisplayName}");
+        }
+
+        nodes = await WearableClass.GetNodeClient(this).GetConnectedNodesAsync();
+        return nodes;
+    }
+    */
+
+
+    #region CapabilityClient.IOnCapabilityChangedListener
+    public void OnCapabilityChanged(ICapabilityInfo capabilityInfo)
+    {
+        System.Diagnostics.Debug.WriteLine("Watch: OnCapabilityChanged");
+
+        nodes = capabilityInfo.Nodes;
+    }
+    #endregion
+
+
+    #region MessageClient.IOnMessageReceivedListener
+    public void OnMessageReceived(IMessageEvent messageEvent)
+    {
+        System.Diagnostics.Debug.WriteLine("Watch: OnMessageReceived");
+        if (messageEvent.Path == "/count")
+        {
+            var data = messageEvent.GetData();
+            count = BitConverter.ToInt32(data);
+            UpdateTextDisplay();
+        }
+    }
+    #endregion
 }
-
-
