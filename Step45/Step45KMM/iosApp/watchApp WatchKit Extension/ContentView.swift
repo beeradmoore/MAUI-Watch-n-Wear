e88c4f25ff4c3d1890366fb1906a4a67387f3b94
Csaba8472 <csab.huszar@gmail.com>
2022-06-25 09:26:12 +0200

    add watchApp target

---
+++ Step45/Step45KMM/iosApp/watchApp WatchKit Extension/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  watchApp WatchKit Extension
+//
+//  Created by Csaba Huszár on 2022. 06. 25..
+//  Copyright © 2022. orgName. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Step45/Step45KMM/iosApp/watchApp WatchKit Extension/iosAppApp.swift
@@ -0,0 +1,20 @@
+//
+//  iosAppApp.swift
+//  watchApp WatchKit Extension
+//
+//  Created by Csaba Huszár on 2022. 06. 25..
+//  Copyright © 2022. orgName. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct iosAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
